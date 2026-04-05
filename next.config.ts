773edde287fb22bf9092cd254038a25d0f1117c1
hasankhalifa2001-glitch <hasan.khalifa2001@gmail.com>
2026-04-05 11:15:52 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "riskless-teddy-doxologically.ngrok-free.dev"
+  ]
 };
 
 export default nextConfig;
